ba19f3cf45fee9732e165b32d55c112e5ca5293a
Arambakam-yaswanth <yyyashu@gmail.com>
2025-05-06 11:49:54 +0530

    create web security group

--- vpc.tf
+++ vpc.tf
@@ -183,3 +183,37 @@ resource "aws_network_acl_association" "lms-db-nacl-asc" {
   network_acl_id = aws_network_acl.lms-db-nacl.id
   subnet_id      = aws_subnet.lms-database-subnet.id
 }
+# lms web security_group
+resource "aws_security_group" "lms-web-sg" {
+  name        = "lms-web-sg"
+  description = "Allow SSH & HTTP traffic"
+  vpc_id      = aws_vpc.lms-vpc.id
+
+  tags = {
+    Name = "lms-web-sg"
+  }
+}
+
+# lms web security_group ingress
+resource "aws_vpc_security_group_ingress_rule" "lms-web-sg-ingress-ssh" {
+  security_group_id = aws_security_group.lms-web-sg.id
+  cidr_ipv4         = 0.0.0.0/0
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lms-web-sg-ingress-ssh" {
+  security_group_id = aws_security_group.lms-web-sg.id
+  cidr_ipv4         = 0.0.0.0/0
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+# lms web security_group egress
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.lms-web-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
