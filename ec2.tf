dc17d262cfec1af257171eea5e7a1c92421ce03d
Arambakam-yaswanth <yyyashu@gmail.com>
2025-05-06 13:47:00 +0530

    Update

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "lms-web-server" {
   ami           = "ami-075686beab831bb7f"
   instance_type = "t2.micro"
-  subnet_id = aws_subnet" "lms-web-subnet.id
+  subnet_id = aws_subnet.lms-web-subnet.id
   key_name = "Oregon"
 vpc_security_group_ids = [aws_security_group" "lms-web-sg.id]
 user_data = file("setup.sh")
