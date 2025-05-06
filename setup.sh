a79c819581f5c9e31814ade9bc5e5be978928acd
Arambakam-yaswanth <yyyashu@gmail.com>
2025-05-06 13:52:15 +0530

    update @3

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 sudo apt update -y
 sudo apt install nginx -y
 sudo rm -rf /var/www/html/*
-sudo git clone https://github.com/Arambakam-yaswanth/ecomm-10am.git /var/www/html
+sudo git clone https://github.com/Arambakam-yaswanth/login-2433.git /var/www/html
